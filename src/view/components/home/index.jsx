694713d22b2b30a0d5ef363e1541b8643ebdbf03
cunhafelipe <felipe.cunha2001@hotmail.com>
2024-06-24 20:09:12 -0300

    fix: ajustment in home

--- src/view/components/home/index.jsx
+++ src/view/components/home/index.jsx
@@ -2,36 +2,31 @@ import React from "react";
 import { motion, useInView } from "framer-motion";
 
 export default function Home() {
-  const ref1 = React.useRef(null);
   const ref2 = React.useRef(null);
   const ref3 = React.useRef(null);
   const ref4 = React.useRef(null);
   const ref5 = React.useRef(null);
 
-  const inView1 = useInView(ref1, { triggerOnce: true });
   const inView2 = useInView(ref2, { triggerOnce: true });
   const inView3 = useInView(ref3, { triggerOnce: true });
   const inView4 = useInView(ref4, { triggerOnce: true });
   const inView5 = useInView(ref5, { triggerOnce: true });
 
   return (
-    <div className="h-[650px] md:h-[800px] w-full bg-gradient-to-br p-2 -mt-2 bg-white">
-      <div className="flex justify-center md:mt-36">
-        <motion.img
-          ref={ref1}
-          src="/oculos-virtual.png"
-          alt=""
-          className="relative right-[500px]"
-          width={700}
-          initial={{ opacity: 0, x: -200 }}
-          animate={inView1 ? { opacity: 1, x: 0 } : {}}
-          transition={{ duration: 1 }}
-        />
+    <div
+      className="h-[650px] md:h-[800px]  bg-gradient-to-br p-2 -mt-2 bg-white "
+      style={{
+        backgroundImage: 'url("/public/image-home.jpg")',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="flex justify-end md:mt-36">
         <div className="z-50 absolute mt-2 md:mt-0">
           <div className="flex flex-col text-center">
             <motion.h1
               ref={ref2}
-              className="md:text-5xl text-3xl text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-indigo-900 font-semibold"
+              className="md:text-5xl text-3xl  text-white font-semibold"
               initial={{ opacity: 0, y: -50 }}
               animate={inView2 ? { opacity: 1, y: 0 } : {}}
               transition={{ duration: 1, delay: 0.2 }}
@@ -40,7 +35,7 @@ export default function Home() {
             </motion.h1>
             <motion.span
               ref={ref3}
-              className="md:text-3xl text-xl  font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-indigo-900"
+              className="md:text-3xl text-xl  font-semibold text-white"
               initial={{ opacity: 0, y: -50 }}
               animate={inView3 ? { opacity: 1, y: 0 } : {}}
               transition={{ duration: 1, delay: 0.2 }}
@@ -48,10 +43,10 @@ export default function Home() {
               Acadêmicos e profissionais da saúde:
             </motion.span>
           </div>
-          <div className="flex flex-col md:mt-16 mt-10 gap-4 p-2">
+          <div className="flex flex-col md:mt-16 mt-10 gap-4 p-2 w-[1000px]">
             <motion.span
               ref={ref4}
-              className="bg-pink-600 md:text-2xl text-lg text-white font-semibold p-4 rounded-md w-full"
+              className="bg-pink-600 md:text-2xl text-lg text-white font-semibold p-4 rounded-sm w-full"
               initial={{ opacity: 0, y: 50 }}
               animate={inView4 ? { opacity: 1, y: 0 } : {}}
               transition={{ duration: 1, delay: 0.2 }}
@@ -61,7 +56,7 @@ export default function Home() {
             </motion.span>
             <motion.span
               ref={ref5}
-              className="bg-pink-600 md:text-2xl text-lg text-white font-semibold p-4 rounded-md md:w-2/3"
+              className="bg-pink-600 md:text-2xl text-lg text-white font-semibold p-4 rounded-sm md:w-2/3"
               initial={{ opacity: 0, y: 50 }}
               animate={inView5 ? { opacity: 1, y: 0 } : {}}
               transition={{ duration: 1, delay: 0.2 }}
@@ -70,7 +65,7 @@ export default function Home() {
               alcançar seus objetivos mais ambiciosos.
             </motion.span>
             <motion.span
-              className="bg-pink-600 md:text-2xl text-lg text-white font-semibold p-4 rounded-md md:w-2/4"
+              className="bg-pink-600 md:text-2xl text-lg text-white font-semibold p-4 rounded-sm md:w-2/4"
               initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 1, delay: 0.2 }}
