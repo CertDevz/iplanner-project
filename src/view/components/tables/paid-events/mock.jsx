eb24e12e8952924b9b1e92fbe90532535aa4917e
cunhafelipe <felipe.cunha2001@hotmail.com>
2024-06-25 19:17:38 -0300

    refactor: creating mock in table paid events

---
+++ src/view/components/tables/paid-events/mock.jsx
@@ -0,0 +1,50 @@
+export const dados = [
+  {
+    funcionalidade: "Gestão de inscrição",
+    descricao: "Permite aos organizadores gerenciar incrições",
+    limitacao: "Incluída",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Capacidade máxima",
+    descricao: "Número máximo de participantes",
+    limitacao: "Até 50 participantes",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Personalização de site",
+    descricao: "Criação de uma página dedicada ao evento",
+    limitacao: "Incluída",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Tempo de recebimento de fundos",
+    descricao: "Tempo para recebimento dos pagamentos",
+    limitacao: "2 dias",
+    custo: "-",
+  },
+  {
+    funcionalidade: "Transmissão ao vivo",
+    descricao: "Plataforma de streaming para eventos",
+    limitacao: "Até 300 parcipantes",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Ferramente da e-mail",
+    descricao: "Envio de e-mails para participantes",
+    limitacao: "Até 10 envios",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Atividade e worksshops",
+    descricao: "Incrição para atividades específicas",
+    limitacao: "Incluída",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Submissão de trabalhos",
+    descricao: "Envio e avaliação de trabalhos acadêmicos",
+    limitacao: "Incluída",
+    custo: "Grátis",
+  },
+];
