82bbc9844949a8154f54954169891ec3eb0bcc1a
cunhafelipe <felipe.cunha2001@hotmail.com>
2024-06-25 19:15:03 -0300

    refactor:creating mock in table free

---
+++ src/view/components/tables/events-free/mock.jsx
@@ -0,0 +1,68 @@
+export const dados = [
+  {
+    funcionalidade: "Gestão de inscrição",
+    descricao: "Permite aos organizadores gerenciar inscrições",
+    limitacao: "Incluída",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Capacidade máxima",
+    descricao: "Número máximo de participantes",
+    limitacao: "Até 50 participantes",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Personalização de site",
+    descricao: "Criação de uma página dedicada ao evento",
+    limitacao: "Incluída",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Ferramenta de e-mail",
+    descricao: "Envio de e-mails para participantes",
+    limitacao: "Verificar disponibilidade",
+    custo: "-",
+  },
+  {
+    funcionalidade: "Submissão de trabalhos",
+    descricao: "Envio e avaliação de trabalhos acadêmicos",
+    limitacao: "Verificar disponibilidade",
+    custo: "-",
+  },
+  {
+    funcionalidade: "Processo de credenciamento",
+    descricao: "Verificação de validação de participantes",
+    limitacao: "Incluído",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Monitoramento de presença",
+    descricao: "Controle de entrada e saída dos participantes",
+    limitacao: "Incluído",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Ferramenta de sorteios",
+    descricao: "Realização de sorteios durante o evento",
+    limitacao: "Incluída",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Emissão de certificados",
+    descricao: "Emissão de certificados de participação",
+    limitacao: "Incluída",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Pesquisa de feedback",
+    descricao: "Coleta de feedback pós-evento",
+    limitacao: "Incluída",
+    custo: "Grátis",
+  },
+  {
+    funcionalidade: "Relatórios analíticos",
+    descricao: "Relatórios detalhados sobre o evento",
+    limitacao: "Incluídos",
+    custo: "Grátis",
+  },
+];
