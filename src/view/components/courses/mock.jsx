9e57ac7e3a9b0d967d44952cb1a2e7ef38160052
cunhafelipe <felipe.cunha2001@hotmail.com>
2024-06-25 19:08:33 -0300

    refactor: creating mock in courses

---
+++ src/view/components/courses/mock.jsx
@@ -0,0 +1,29 @@
+// src/components/CourseMock.js
+
+export const courses = [
+  {
+    image: "/courses/restauration.jpg",
+    title: "Curso restauração florestal",
+    description: "25 JUN 2024",
+  },
+  {
+    image: "/courses/developer.jpg",
+    title: "Curso Desenvolvimento Web",
+    description: "25 JUN 2024",
+  },
+  {
+    image: "/courses/design.jpg",
+    title: "Curso Design Gráfico",
+    description: "25 JUN 2024",
+  },
+  {
+    image: "/courses/instagram.jpg",
+    title: "Curso Instagram para Negócios",
+    description: "25 JUN 2024",
+  },
+  {
+    image: "/courses/seminario.jpg",
+    title: "Ações para o Combate à Evasão",
+    description: "25 JUN 2024",
+  },
+];
