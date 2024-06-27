4af259316a655ca6db182e956ef739592b19f191
cunhafelipe <felipe.cunha2001@hotmail.com>
2024-06-27 15:21:22 -0300

    fix: ajustment in design

--- src/view/components/Solution/index.jsx
+++ src/view/components/Solution/index.jsx
@@ -29,7 +29,7 @@ export default function Solution() {
           "linear-gradient(to bottom right, #7c3aed 20%, #3d44c7 100%)",
       }}
     >
-      <img src="/public/medicos-visao.png" alt="" className="hidden md:block" />
+      <img src="/medicos-visao.png" alt="" className="hidden md:block" />
       <div className="flex flex-col max-w-[900px] mx-auto md:mt-10 items-center mb-10">
         <h1 className="font-semibold px-5 py-2 bg-[#db2777] rounded-sm text-white text-center md:text-3xl text-2xl -mt-2 md:mt-0">
           Serviço de Gestão de Eventos Completo para a iplanner
