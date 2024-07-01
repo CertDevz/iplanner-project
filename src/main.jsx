a575625a27464811ac4e466d4bcad802435a4de4
Rafael Torreson <rafael.torreson@hotmail.com>
2024-07-01 19:57:49 -0300

    fix: lint code

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,32 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import WeAre from "./view/components/weare";
-import Constructor from "./view/components/constructor";
-import PageEvents from "./view/components/page-events";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import WeAre from './view/components/weare';
+import Constructor from './view/components/constructor';
+import PageEvents from './view/components/page-events';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
   },
   {
-    path: "/quem-somos",
+    path: '/quem-somos',
     element: <WeAre />,
   },
   {
-    path: "/constructor",
+    path: '/constructor',
     element: <Constructor />,
   },
   {
-    path: "/evento/:id/informacoes",
+    path: '/evento/:id/informacoes',
     element: <PageEvents />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
