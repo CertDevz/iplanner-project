b796e42f5cf77f2623b7c7232c97145e1e49445c
cunhafelipe <felipe.cunha2001@hotmail.com>
2024-07-10 14:35:00 -0300

    fix: mock causing problem with id

--- src/view/components/courses/mock.jsx
+++ src/view/components/courses/mock.jsx
@@ -1,30 +1,30 @@
 export const courses = [
-  {
-    image: "/courses/carreira.jpeg",
-    title:
-      "Inteligência Emocional para Gestores: Lidere com Empatia e Eficiência",
-    description: "25 JUN 2024",
-  },
-  {
-    image: "/courses/inteligencia.jpeg",
-    title: "Acelere sua Carreira com as Técnicas Mais Eficazes de Gestão",
-    description: "25 JUN 2024",
-  },
-  {
-    image: "/courses/saude.jpeg",
-    title:
-      "Transforme Realidades com Saúde Coletiva: Seja um Agente de Mudança",
-    description: "25 JUN 2024",
-  },
-  {
-    image: "/courses/transforme.jpeg",
-    title:
-      "Saúde Coletiva: A Base para uma Saúde de Qualidade - Atualização para Profissionais",
-    description: "25 JUN 2024",
-  },
   // {
-  //   image: "/courses/seminario.jpg",
-  //   title: "Ações para o Combate à Evasão",
+  //   image: "/courses/carreira.jpeg",
+  //   title:
+  //     "Inteligência Emocional para Gestores: Lidere com Empatia e Eficiência",
   //   description: "25 JUN 2024",
   // },
+  // {
+  //   image: "/courses/inteligencia.jpeg",
+  //   title: "Acelere sua Carreira com as Técnicas Mais Eficazes de Gestão",
+  //   description: "25 JUN 2024",
+  // },
+  // {
+  //   image: "/courses/saude.jpeg",
+  //   title:
+  //     "Transforme Realidades com Saúde Coletiva: Seja um Agente de Mudança",
+  //   description: "25 JUN 2024",
+  // },
+  // {
+  //   image: "/courses/transforme.jpeg",
+  //   title:
+  //     "Saúde Coletiva: A Base para uma Saúde de Qualidade - Atualização para Profissionais",
+  //   description: "25 JUN 2024",
+  // },
+  // // {
+  // //   image: "/courses/seminario.jpg",
+  // //   title: "Ações para o Combate à Evasão",
+  // //   description: "25 JUN 2024",
+  // // },
 ];
